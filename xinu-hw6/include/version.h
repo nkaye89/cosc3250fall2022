e8649d54af87a6e0a440515286a06f9ecbda699d
nkaye89 <noah.kaye@marquette.edu>
2022-10-31 16:12:11 -0500

    solved infinite loop

--- xinu-hw6/include/version.h
+++ xinu-hw6/include/version.h
@@ -1,1 +1,1 @@
-#define VERSION "(Embedded Xinu) (arm-rpi3) #104 (nkaye@morbius) Mon Oct 31 10:20:40 CDT 2022"
+#define VERSION "(Embedded Xinu) (arm-rpi3) #109 (nkaye@morbius) Mon Oct 31 16:06:27 CDT 2022"
